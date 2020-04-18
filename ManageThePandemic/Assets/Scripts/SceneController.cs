93a5cadc8699714d12f615475b4b17023dc5853d
Barış ALHAN <barisalhan003@gmail.com>
2020-04-18 22:43:33 +0300

    Start scene is created.

---
+++ ManageThePandemic/Assets/Scripts/SceneController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void OnStartButton()
+    {
+        Debug.Log("Game is started.");
+        SceneManager.LoadScene("GameScreen");
+    }
+
+    public void OnLoadButton()
+    {
+        
+    }
+}
