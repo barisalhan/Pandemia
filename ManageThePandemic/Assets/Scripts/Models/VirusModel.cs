3543842dd66579e95542b213a486eaadb30a2a1f
Barış ALHAN <barisalhan003@gmail.com>
2020-04-28 19:00:31 +0300

    Theme of the game is changed.

--- ManageThePandemic/Assets/Scripts/Models/VirusModel.cs
+++ ManageThePandemic/Assets/Scripts/Models/VirusModel.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Reflection;
-using System.Threading;
-using UnityEditorInternal;
 using UnityEngine;
-using UnityEngine.Experimental.Audio.Google;
 using Object = System.Object;
 
 
