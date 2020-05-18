0f683daa39d61ca3bda2252789302cd4fed75d02
Barış ALHAN <barisalhan003@gmail.com>
2020-05-18 17:39:37 +0300

    Hopefully it is the last push.

---
+++ ManageThePandemic/Assets/ButtonController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+
+    public List<Button> buttons = new List<Button>();
+
+    public GameObject country;
+
+    Button[] buttonsCountry;
+    public void Awake()
+    {
+        buttonsCountry = country.GetComponentsInChildren<Button>();
+    }
+
+    public void EnableButtons()
+    {
+        foreach (Button button in buttons)
+        {
+            button.interactable = true;
+        }
+        foreach (Button button in buttonsCountry)
+        {
+            button.interactable = true;
+        }
+    }
+
+    public void DisableButtons()
+    {
+        foreach (Button button in buttons)
+        {
+            button.interactable = false;
+        }
+        foreach (Button button in buttonsCountry)
+        {
+            button.interactable = false;
+        }
+    }
+}
